042321d4059cf279dccf8fa8dad3c18dd0604fe0
SrAlch94 <daniel.contero@gmail.com>
2022-04-27 21:45:40 +0100

    Styled-components test

--- src/components/Grid/Grid.styles.ts
+++ src/components/Grid/Grid.styles.ts
@@ -1,4 +1,26 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
-export const Content = styled.div``;
+export const Wrapper = styled.div`
+    background: rgba(0, 0, 0 , 0.5);
+    position: relative;
+    width: 100vw;
+    height: 80px;
+    z-index: 1;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    margin: 0 auto;
+
+    .routes {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        max-width: 800px;
+        min-width: 400px;
+        height: 100%;
+    }
+`;
